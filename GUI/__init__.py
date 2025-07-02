2e64b3020241a3ce93a46e0660c74e81825d2ee5
dannyzimm <daniel.prz.rodriguez@gmail.com>
2025-07-02 14:49:05 +0200

    Add UI imports module, update app.ui properties, and include resources for icons and screenshots

---
+++ GUI/__init__.py
@@ -0,0 +1,8 @@
+"""
+Auto-generated UI imports module.
+This file imports all compiled UI files for easier access.
+"""
+
+from GUI.insert_form_ui import *
+from GUI.about_ui import *
+from GUI.app_ui import *
